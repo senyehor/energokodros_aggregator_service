d988ea8e6e866322603799756cd938f243e2a89d
Yehor Sencha <senyehor@gmail.com>
2023-04-17 12:29:04 +0300

    ref: remade controller to use new state receiver

--- aggregator_controller/controller.py
+++ aggregator_controller/controller.py
@@ -1,38 +1,33 @@
+import logging
+
+from aggregator_controller.exceptions import AggregationAlreadyRunning
 from aggregator_controller.states import AggregatorStates
 from utils.commands import Command
-from utils.current_state_retreiver import (
-    RawCurrentStateRetriever,
-    try_convert_raw_state_with_correct_exception,
-)
+from utils.redis.state import StateReceiver
+
+log = logging.getLogger(__name__)
 
 
-class AggregatorController:  # todo mb make abc for state
+class AggregatorController:
     __aggregator_state: AggregatorStates
 
     def __init__(
-            self, aggregator_state_receiver: RawCurrentStateRetriever,
+            self, aggregator_state_receiver: StateReceiver,
             start_aggregation_command: Command
     ):
-        self.__state_receiver = aggregator_state_receiver
+        self.__aggregator_state_receiver = aggregator_state_receiver
         self.__start_aggregation_command = start_aggregation_command
 
     def start_aggregation(self):
+        self.__update_aggregator_state()
         if self.__is_aggregator_running:
-            return
+            raise AggregationAlreadyRunning
         self.__start_aggregation_command.execute()
 
     @property
     def __is_aggregator_running(self) -> bool:
         return self.__aggregator_state == AggregatorStates.RUNNING
 
-    @property
-    def __is_aggregator_idle(self) -> bool:
-        return self.__aggregator_state == AggregatorStates.IDLE
-
-    def __update_current_state(self):
-        raw_current_state = self.__state_receiver.get_current_raw_state()
-        current_state = try_convert_raw_state_with_correct_exception(
-            raw_current_state,
-            AggregatorStates
-        )
-        self.__aggregator_state = current_state
+    def __update_aggregator_state(self):
+        # noinspection PyTypeChecker
+        self.__aggregator_state = self.__aggregator_state_receiver.get_state()
