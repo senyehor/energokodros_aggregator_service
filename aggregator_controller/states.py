e5b55ce68caef001d6410f2a162c8a3e6e83d0a2
Yehor Sencha <senyehor@gmail.com>
2023-04-17 12:19:13 +0300

    feat: use new states base

--- aggregator_controller/states.py
+++ aggregator_controller/states.py
@@ -1,6 +1,6 @@
-from enum import StrEnum
+from utils.redis.state import StatesBase
 
 
-class AggregatorStates(StrEnum):
+class AggregatorStates(StatesBase):
     RUNNING = 'running'
     IDLE = 'idle'
