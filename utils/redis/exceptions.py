9f68c0b999a77bffdeaa5d3d6bdccd917f78d30a
Yehor Sencha <senyehor@gmail.com>
2023-04-17 13:54:32 +0300

    fix: removed __str__ as now it is in base class

--- utils/redis/exceptions.py
+++ utils/redis/exceptions.py
@@ -9,11 +9,6 @@ class InvalidReceiversCount(LogicExceptionWithMessage):
     def __init__(self, actual_count: int, channel_name: str):
         self.message = f'message to channel {channel_name} expected 1 receiver, got {actual_count}'
 
-    def __str__(self):
-        return f'{super().__str__()}' \
-               f'\n' \
-               f'{self.message}'
-
 
 class InvalidChannelSubscriberCount(LogicException):
     pass
