4d1925a4649da6937bbc237e93148e905ef2300b
Yehor Sencha <senyehor@gmail.com>
2023-04-17 12:51:23 +0300

    feat: removed redundant commands and fixed channel names

--- box_listening_suspender/commands.py
+++ box_listening_suspender/commands.py
@@ -1,29 +1,18 @@
 from box_listening_suspender.config import BOX_LISTENER_SUSPENDER_CONFIG
 from settings import REDIS
-from utils.redis.commands import GetRedisMessageCommand, SendRedisMessageToOneReceiverCommand
+from utils.redis.commands import SendRedisMessageToOneReceiverCommand
 
 _ = BOX_LISTENER_SUSPENDER_CONFIG
 
 stop_listening = SendRedisMessageToOneReceiverCommand(
-    channel=_.channels.send_channel,
+    channel=_.channel_name,
     message=_.stop_listening_message,
     r=REDIS,
     command_name='stop_box_listening'
 )
 resume_listening = SendRedisMessageToOneReceiverCommand(
-    channel=_.channels.send_channel,
+    channel=_.channel_name,
     message=_.resume_listening_message,
     r=REDIS,
     command_name='resume_box_listening'
-)
-update_box_listener_state = SendRedisMessageToOneReceiverCommand(
-    channel=_.channels.send_channel,
-    message=_.update_state_message,
-    r=REDIS,
-    command_name='update_box_listener_state'
-)
-get_box_listener_state = GetRedisMessageCommand(
-    channel=_.channels.listen_channel,
-    r=REDIS,
-    command_name='get_box_listener_state'
 )
