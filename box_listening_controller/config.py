e315e15fe2dd328e2d9a8ba887dacc94e724c39d
Yehor Sencha <senyehor@gmail.com>
2023-05-02 10:48:19 +0300

    ref: removed dots from env values as they are not set with docker envs if contain dots in names

--- box_listening_controller/config.py
+++ box_listening_controller/config.py
@@ -8,16 +8,20 @@ from utils.config_base import RedisChannelMixin
 class __BoxListenerControllerConfig(RedisChannelMixin):
     stop_listening_message: str
     resume_listening_message: str
+    state_key: str
 
 
 BOX_LISTENER_CONTROLLER_CONFIG = __BoxListenerControllerConfig(
     channel_name=os.getenv(
-        'BOX_LISTENER_CONTROLLER_CONFIG.CHANNEL_NAME'
+        'BOX_LISTENER_CONTROLLER_CONFIG_CHANNEL_NAME'
     ),
     stop_listening_message=os.getenv(
-        'BOX_LISTENER_CONTROLLER_CONFIG.STOP_LISTENING_MESSAGE'
+        'BOX_LISTENER_CONTROLLER_CONFIG_STOP_LISTENING_MESSAGE'
     ),
     resume_listening_message=os.getenv(
-        'BOX_LISTENER_CONTROLLER_CONFIG.RESUME_LISTENING_MESSAGE'
+        'BOX_LISTENER_CONTROLLER_CONFIG_RESUME_LISTENING_MESSAGE'
+    ),
+    state_key=os.getenv(
+        'BOX_LISTENER_CONTROLLER_BOX_LISTENER_STATE_KEY'
     )
 )
--- settings.py
+++ settings.py
@@ -1,5 +1,12 @@
+import os
+
 from redis.client import Redis
 
-REDIS = Redis()
+REDIS = Redis(
+    host=os.getenv('REDIS_HOST'),
+    port=int(os.getenv('REDIS_PORT')),
+    db=int(os.getenv('REDIS_DB')),
+    password=os.getenv('REDIS_PASSWORD')
+)
 
 DEFAULT_TIMEOUT_SECONDS = 10
