42b59c984dd4d0bb7c815aedd698a0e01de3fdde
Yehor Sencha <senyehor@gmail.com>
2024-08-04 00:07:47 +0300

    feat: add aggregator_state_key field

--- aggregator_controller/config.py
+++ aggregator_controller/config.py
@@ -7,6 +7,7 @@ from utils.config_base import RedisChannelMixin
 @dataclass
 class __AggregatorControllerConfig(RedisChannelMixin):
     start_aggregation_message: str
+    aggregator_state_key: str
 
 
 AGGREGATOR_CONTROLLER_CONFIG = __AggregatorControllerConfig(
@@ -16,4 +17,7 @@ AGGREGATOR_CONTROLLER_CONFIG = __AggregatorControllerConfig(
     start_aggregation_message=os.getenv(
         'AGGREGATOR_CONTROLLER_CONFIG.START_AGGREGATION_MESSAGE'
     ),
+    aggregator_state_key=os.getenv(
+        'AGGREGATOR_CONTROLLER_CONFIG.AGGREGATOR_STATE_KEY'
+    )
 )
