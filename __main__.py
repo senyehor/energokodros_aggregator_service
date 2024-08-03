f3f4084d8083caeedd9f04ea746915528dd9609e
Yehor Sencha <senyehor@gmail.com>
2024-08-04 01:33:07 +0300

    feat: add AggregationAlreadyRunning exception catch

--- __main__.py
+++ __main__.py
@@ -1,10 +1,15 @@
+import logging
+
 from dotenv import load_dotenv
 
 from aggregator_controller import create_redis_aggregator_controller
+from aggregator_controller.exceptions import AggregationAlreadyRunning
 from box_listening_controller import create_redis_box_listener_controller
 from start_aggregation_request_listener import create_redis_start_aggregation_request_listener
 
 load_dotenv()
+
+log = logging.getLogger(__name__)
 
 
 def main():
@@ -14,7 +19,10 @@ def main():
     while True:
         if start_aggregation_request_listener.check_start_aggregation_request_came():
             with box_listening_suspender:
-                aggregator_controller.start_aggregation()
+                try:
+                    aggregator_controller.start_aggregation()
+                except AggregationAlreadyRunning as e:
+                    log.warning(e.message)
         else:
             start_aggregation_request_listener.sleep_for_request_check_delay()
 
