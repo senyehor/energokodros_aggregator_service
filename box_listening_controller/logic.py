a599db4731e412726ac55c18cb230cf1d1edf85e
Yehor Sencha <senyehor@gmail.com>
2024-08-21 01:47:13 +0300

    fix: add forgotten parameter

--- box_listening_controller/logic.py
+++ box_listening_controller/logic.py
@@ -1,6 +1,9 @@
-from box_listening_controller.commands import resume_listening, stop_listening
+from box_listening_controller.commands import (
+    get_box_listener_state, resume_listening,
+    stop_listening,
+)
 from box_listening_controller.controller import BoxListenerController
 
 
 def create_redis_box_listener_controller() -> BoxListenerController:
-    return BoxListenerController(stop_listening, resume_listening)
+    return BoxListenerController(stop_listening, resume_listening, get_box_listener_state)
