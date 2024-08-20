39a072be3cf8dbe11316478ad4e96703b5a209b7
Yehor Sencha <senyehor@gmail.com>
2024-08-20 23:11:01 +0300

    feat: remade state receiver to accept state factory as init parameter

--- utils/redis/state.py
+++ utils/redis/state.py
@@ -1,20 +1,21 @@
 from enum import StrEnum
-from typing import Any, Callable
+from typing import Any, Callable, TypeAlias
 
 from utils.commands import Command
 from utils.redis.exceptions import InvalidRawState
 
-StatesBase = StrEnum
+StatesBase: TypeAlias = StrEnum
 
 
 class StateReceiver:
 
-    def __init__(self, get_state: Command):
+    def __init__(self, get_state: Command, state_maker: Callable[[str], Any]):
         self.__get_state = get_state
+        self.__state_maker = state_maker
 
-    def get_state(self, state_maker: Callable[[str], Any]) -> StatesBase:
+    def get_state(self) -> StatesBase:
         raw_state = self.__get_state.execute()
         try:
-            return state_maker(raw_state)
+            return self.__state_maker(raw_state)
         except ValueError as e:
             raise InvalidRawState(raw_state) from e
