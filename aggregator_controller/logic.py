351809cf338a9c04381b1aa2ca02e507354f7662
Yehor Sencha <senyehor@gmail.com>
2024-08-20 23:12:17 +0300

    fix: changes due to state receiver redesign

--- aggregator_controller/logic.py
+++ aggregator_controller/logic.py
@@ -2,11 +2,12 @@ from aggregator_controller.commands import (
     get_aggregator_state, start_aggregation,
 )
 from aggregator_controller.controller import AggregatorController
+from aggregator_controller.states import AggregatorStates
 from utils.redis.state import StateReceiver
 
 
 def create_redis_aggregator_controller() -> AggregatorController:
-    aggregator_state_receiver = StateReceiver(get_aggregator_state)
+    aggregator_state_receiver = StateReceiver(get_aggregator_state, AggregatorStates)
     return AggregatorController(
         aggregator_state_receiver,
         start_aggregation
