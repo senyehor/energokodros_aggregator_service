3a862cf970bd3407b1a6a956518760ff9c5a7d0a
Yehor Sencha <senyehor@gmail.com>
2024-08-21 21:37:12 +0300

    fix: pass state receiver instead of get_state command

--- box_listening_controller/logic.py
+++ box_listening_controller/logic.py
@@ -1,9 +1,14 @@
+from box_listening_controller.box_listener_states import BoxListenerStates
 from box_listening_controller.commands import (
     get_box_listener_state, resume_listening,
     stop_listening,
 )
 from box_listening_controller.controller import BoxListenerController
+from utils.redis.state import StateReceiver
 
 
 def create_redis_box_listener_controller() -> BoxListenerController:
-    return BoxListenerController(stop_listening, resume_listening, get_box_listener_state)
+    return BoxListenerController(
+        stop_listening, resume_listening,
+        StateReceiver(get_box_listener_state, BoxListenerStates)
+    )
