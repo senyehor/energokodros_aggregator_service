6d32cc91d8f299c43e6a540cfb6ce54e93800bee
Yehor Sencha <senyehor@gmail.com>
2024-08-21 21:36:44 +0300

    fix: add needed commands to init

--- start_aggregation_request_communicator/logic.py
+++ start_aggregation_request_communicator/logic.py
@@ -1,8 +1,13 @@
 from start_aggregation_request_communicator.commands import (
-    get_start_aggregation_request_message_from_redis
+    get_start_aggregation_request_message_from_redis, notify_aggregation_failed,
+    notify_aggregation_started_or_completed_successfully,
 )
-from start_aggregation_request_communicator.listener import StartAggregationRequestCommunicator
+from start_aggregation_request_communicator.communicator import StartAggregationRequestCommunicator
 
 
 def create_redis_start_aggregation_request_communicator() -> StartAggregationRequestCommunicator:
-    return StartAggregationRequestCommunicator(get_start_aggregation_request_message_from_redis)
+    return StartAggregationRequestCommunicator(
+        get_start_aggregation_request_message_from_redis,
+        notify_aggregation_failed,
+        notify_aggregation_started_or_completed_successfully
+    )
