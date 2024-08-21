d7885475057c137b1c0192a28ecad98e60a48ddc
Yehor Sencha <senyehor@gmail.com>
2024-08-21 21:35:17 +0300

    fix: add command to controller creation

--- aggregator_controller/logic.py
+++ aggregator_controller/logic.py
@@ -1,5 +1,5 @@
 from aggregator_controller.commands import (
-    get_aggregator_state, start_aggregation,
+    get_aggregation_last_time_run, get_aggregator_state, start_aggregation,
 )
 from aggregator_controller.controller import AggregatorController
 from aggregator_controller.states import AggregatorStates
@@ -10,5 +10,6 @@ def create_redis_aggregator_controller() -> AggregatorController:
     aggregator_state_receiver = StateReceiver(get_aggregator_state, AggregatorStates)
     return AggregatorController(
         aggregator_state_receiver,
-        start_aggregation
+        start_aggregation,
+        get_aggregation_last_time_run
     )
